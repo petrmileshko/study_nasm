f960eeab0146c40f0fbfc4e7bcfbe1fd56e3f22e
Peter <mileshko.petr@yandex.ru>
2026-02-17 21:53:53 +0300

    Update hello_printf.asm

--- asm_1/hello_printf.asm
+++ asm_1/hello_printf.asm
@@ -1,22 +1,22 @@
 ; Вариант с использованием функции printf из стандартной бибилиотеки Си
 
-section .data
-    fmt db '%s', 0
-    msg db 'Hello, World!', 0Ah, 0
+section  .data
+         fmt       db '%s', 0
+         msg       db 'Hello, World!', 0Ah, 0
 
-section .text
-    extern printf
-    extern ExitProcess
-    global main
+section  .text
+         extern    printf
+         extern    ExitProcess
+         global    main
 
 main:
-    sub rsp, 8
+         sub       rsp, 8
 
     ; Выводим строку через printf
-    mov rcx, fmt      ; 1-й параметр (формат)
-    mov rdx, msg     ; 2-й параметр (строка)
-    call printf
+         mov       rcx, fmt                      ; 1-й параметр (формат)
+         mov       rdx, msg                      ; 2-й параметр (строка)
+         call      printf
 
     ; Завершаем процесс
-    xor rcx, rcx
-    call ExitProcess
+         xor       rcx, rcx
+         call      ExitProcess
